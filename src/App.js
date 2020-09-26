c3c388ce2142d1fdcae44799b67615567ff57362
SeungminKim <ksm0057@naver.com>
2020-09-26 16:27:19 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-import Name from "./Name";
+
+function Name({ l_name }) {
+  return <h1>My name is {l_name}</h1>;
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
-      <Name />
+      <Name l_name="Kim" />
+      <Name l_name="Lee" />
+      <Name l_name="Park" />
     </div>
   );
 }
