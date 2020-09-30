86ea9d26020637fad5d79dff961b5d67724f29b4
SeungminKim <ksm0057@naver.com>
2020-09-30 19:11:28 +0900

    proptype

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Name({ l_name }) {
-  return <h1>My name is {l_name}</h1>;
+function Name({ l_name, name }) {
+  return (
+    <h1>
+      My name is {name} {l_name}
+    </h1>
+  );
 }
 
 const names = [
   {
     id: 1,
-    name: "kim",
+    last_name: "kim",
+    name: "seo",
   },
   {
     id: 2,
-    name: "lee",
+    last_name: "lee",
+    name: "min",
   },
   {
     id: 3,
-    name: "park",
+    last_name: "park",
+    name: "jun",
   },
   {
     id: 4,
-    name: "seo",
+    last_name: "seo",
+    name: "chang",
   },
 ];
 
@@ -27,7 +36,7 @@ function App() {
   return (
     <div className="App">
       {names.map((f_name) => (
-        <Name key={f_name.id} l_name={f_name.name} />
+        <Name key={f_name.id} l_name={f_name.last_name} name={f_name.name} />
       ))}
     </div>
   );
