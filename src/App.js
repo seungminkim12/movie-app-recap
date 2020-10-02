05596f7342380045e2b0b469aa079b0054c74144
SeungminKim <ksm0057@naver.com>
2020-10-02 18:47:48 +0900

    class component

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Name({ l_name, name }) {
-  return (
-    <h1>
-      My name is {name} {l_name}
-    </h1>
-  );
-}
-
-const names = [
-  {
-    id: 1,
-    last_name: "kim",
-    name: "seo",
-  },
-  {
-    id: 2,
-    last_name: "lee",
-    name: "min",
-  },
-  {
-    id: 3,
-    last_name: "park",
-    name: "jun",
-  },
-  {
-    id: 4,
-    last_name: "seo",
-    name: "chang",
-  },
-];
-
-Name.propTypes = {
-  l_name: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-};
-
-function App() {
-  return (
-    <div className="App">
-      {names.map((f_name) => (
-        <Name key={f_name.id} l_name={f_name.last_name} name={f_name.name} />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    console.log("ADD");
+  };
+  remove = () => {
+    console.log("REMOVE");
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is{this.state.count}</h1>
+        <button onClick={this.add}>ADD</button>
+        <button onClick={this.remove}>REMOVE</button>
+      </div>
+    );
+  }
 }
 
 export default App;
