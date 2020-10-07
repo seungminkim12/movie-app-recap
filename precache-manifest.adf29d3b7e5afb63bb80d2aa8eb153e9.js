53b33721f9e095ce35114b59dfc1fbc3200321f0
SeungminKim <ksm0057@naver.com>
2020-10-07 19:56:01 +0900

    Updates

---
+++ precache-manifest.adf29d3b7e5afb63bb80d2aa8eb153e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b73579d94db1865c9d8777558d86551",
+    "url": "/movie-app-recap/index.html"
+  },
+  {
+    "revision": "f9519d5f91d224864315",
+    "url": "/movie-app-recap/static/css/main.c408477d.chunk.css"
+  },
+  {
+    "revision": "d8d67a69a8545a50ddd4",
+    "url": "/movie-app-recap/static/js/2.cfacd7f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app-recap/static/js/2.cfacd7f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9519d5f91d224864315",
+    "url": "/movie-app-recap/static/js/main.eef5d94e.chunk.js"
+  },
+  {
+    "revision": "2d203bc095748661b202",
+    "url": "/movie-app-recap/static/js/runtime-main.d5cb4bf2.js"
+  }
+]);
